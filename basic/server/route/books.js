e677febd1341e8ac64f7a599b1b30aa3cf627c78
Rvijayvergiya <rahul.vijayvergiya@ubisoft.com>
2019-09-13 14:59:32 +0530

    final code

--- basic/server/route/books.js
+++
@@ -1,9 +0,0 @@
-const bookController = require('./../lib/controller/book');
-//const Books = require('./../lib/model/book');
-module.exports = function (express) {
-    var bookRouter = express.Router();
-    
-    bookRouter.get('/', bookController.getAllBooks);
-    bookRouter.get('/count', bookController.getBookCount);
-    return bookRouter;
-}
