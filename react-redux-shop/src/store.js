db30cca61cf8a405db88b602fcb4624eadd6a30c
Rvijayvergiya <rahul.vijayvergiya@ubisoft.com>
2019-10-10 10:22:16 +0530

    compose redux

--- react-redux-shop/src/store.js
+++ react-redux-shop/src/store.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducers , applyMiddleware } from "redux";
+import { createStore, combineReducers , applyMiddleware, compose  } from "redux";
 import booksReducer from "./redux/reducers/book/books";
 import paginationReducer from "./redux/reducers/pagination";
 import cartReducer from "./redux/reducers/cart/cart";
@@ -7,6 +7,9 @@ import thunk from "redux-thunk";
 const rootReducer = combineReducers({
     booksReducer, paginationReducer, cartReducer
 });
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, compose(
+    applyMiddleware(thunk),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  ) );
 export default store;
 
